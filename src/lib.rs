d8b08ec11391c3056d5a6ded32c0cb2970716210
Andrew Straw <strawman@astraw.com>
2021-08-05 21:56:06 +0200

    improve documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,80 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+//! Return the least-squares solution to a linear matrix equation
+//!
+//! The crate implements the linear least squares solution to a linear matrix
+//! equation.
+//!
+//! Characteristics:
+//!
+//! * Linear algebra and types from the [`nalgebra`](https://docs.rs/nalgebra)
+//!   crate.
+//! * Maximum compatibility with the
+//!   [`numpy.linalg.lstsq`](https://numpy.org/doc/stable/reference/generated/numpy.linalg.lstsq.html)
+//!   Python library function.
+//! * No standard library is required (disable the default features to disable
+//!   use of `std`) and no heap allocations. In other words, this can run on a
+//!   bare-metal microcontroller with no OS.
+//!
+//! Example:
+//!
+//! ```rust
+//! use nalgebra::{self as na, OMatrix, OVector, U2};
+//!
+//! let a = OMatrix::<f64, na::Dynamic, U2>::from_row_slice(&[
+//!     1.0, 1.0,
+//!     2.0, 1.0,
+//!     3.0, 1.0,
+//!     4.0, 1.0,
+//! ]);
+//!
+//! let b = OVector::<f64, na::Dynamic>::from_row_slice(&[2.5, 4.4, 6.6, 8.5]);
+//!
+//! let epsilon = 1e-14;
+//! let results = lstsq::lstsq(&a, &b, epsilon).unwrap();
+//!
+//! assert_eq!(results.solution.nrows(), 2);
+//! approx::assert_relative_eq!(results.solution[0], 2.02, epsilon = epsilon);
+//! approx::assert_relative_eq!(results.solution[1], 0.45, epsilon = epsilon);
+//! approx::assert_relative_eq!(results.residuals, 0.018, epsilon = epsilon);
+//! assert_eq!(results.rank, 2);
+//! ```
+
 use nalgebra::allocator::Allocator;
 use nalgebra::base::{OMatrix, OVector};
 use nalgebra::dimension::{Dim, DimDiff, DimMin, DimMinimum, DimSub, U1};
 use nalgebra::{DefaultAllocator, RealField};
 
+/// Results of [lstsq]
 pub struct Lstsq<R: RealField, N: Dim>
 where
     DefaultAllocator: Allocator<R, N>,
 {
+    /// Least-squares solution.
+    ///
+    /// This is the variable `x` that approximatively solves the equation `a * x = b`.
     pub solution: OVector<R, N>,
+    /// Sums of squared residuals: Squared Euclidean 2-norm.
     pub residuals: R,
+    /// Rank of matrix `a`.
     pub rank: usize,
 }
 
 /// Return the least-squares solution to a linear matrix equation.
 ///
+/// Computes the vector x that approximatively solves the equation `a * x = b`.
 /// Usage is maximally compatible with Python's `numpy.linalg.lstsq`.
+///
+/// Arguments:
+///
+/// - `a`: "Coefficient" matrix (shape: M rows, N columns)
+/// - `b`: Ordinate or “dependent variable” values (shape: M dimensional)
+/// - `epsilon`: singular values less than this are assumed to be zero.
+///
+/// Returns:
+/// - `Result<`[Lstsq]`,&'static str>`
+///
+/// See the module level documentation for example of usage.
 pub fn lstsq<R, M, N>(
     a: &OMatrix<R, M, N>,
     b: &OVector<R, M>,
